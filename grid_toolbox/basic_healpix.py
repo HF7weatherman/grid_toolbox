427313797b421c766bf5f4ec3003e83a9a04c63c
Henning Franke <henning.franke@mpimet.mpg.de>
2025-05-21 16:41:32 +0200

    Add functions from Lukas Brunner (UHH) to coarsen resolution of Healpix grid.

--- grid_toolbox/basic_healpix.py
+++ grid_toolbox/basic_healpix.py
@@ -70,6 +70,130 @@ def _ring2nest_index(var: xr.DataArray, nside: int) -> np.ndarray:
     return np.array([hp.nest2ring(nside, i) for i in var.cell.values])
 
 
+def coarsen_hp_grid_xr(
+        da: xr.DataArray,
+        z_out: int,
+        method: str='mean',
+        gridn=None
+        ) -> xr.DataArray:
+    """
+    Thin xarray wrapper for `aggregate_grid'. Developed by Lukas Brunner, UHH
+    """
+    
+    if gridn is None:  # try to guess grid name from frequent options
+        gridn = _guess_gridn(da)
+            
+    return xr.apply_ufunc(
+        _coarsen_hp_grid,
+        da, z_out,
+        input_core_dims=[[gridn], []],
+        output_core_dims=[['tmp']],
+        vectorize=True,
+        kwargs={'method': method},
+    ).rename({'tmp': gridn})
+
+
+def _coarsen_hp_grid(
+        arr: np.ndarray,
+        z_out: int,
+        method: str='mean'
+        ) -> np.ndarray:
+    """Spatially aggregate to a coarser grid. Developed by Lukas Brunner, UHH.
+
+    Parameters
+    ----------
+    arr : np.ndarray, shape (M,)
+        The length of the array M has to be M = 12 * (2**zoom)**2
+    z_out : int
+        Healpix zoom level of the output grid. Needs to be smaller than the input zoom level.
+    method : str, optional by default 'mean'
+        Spatial aggregation method. 
+        - 'mean': Mean of sub-grid cells -> conservative regridding
+                  This is equivalent to `healpy.ud_grade`
+        - 'std': Standard deviation of sub-grid cells
+        - 'min': Minimum of sub-grid cells
+        - 'max': Maximum of sub-grid cells
+
+    Returns
+    -------
+    np.ndarray, shape (N < M,)
+
+    Info
+    ----
+    Zoom levels in healpix (Hierarchical Equal Area isoLatitude Pixelization of a sphere)
+    https://healpy.readthedocs.io/en/latest/index.html
+    https://healpix.jpl.nasa.gov/index.shtml
+
+    Zoom level 0 divides the globe into 12 grid cells, each zoom level
+    increase increases the number of cells by 4 and half the resolution
+    in kilometer
+
+    nside = 2**zoom
+    nr. cells = 12 * nside**2
+
+    | zoom | nside | res. (km) | nr. cells  |
+    | ---- | ----- | --------- | ---------- |
+         0 |     1 |    6519.6 | 12
+         1 |     2 |    3259.8 | 48
+         2 |     4 |    1629.9 | 192
+         3 |     8 |     815.0 | 768
+         4 |    16 |     407.5 | 3,072
+         5 |    32 |     203.7 | 12,288
+         6 |    64 |     101.9 | 49,152
+         7 |   128 |      50.9 | 196,608
+         8 |   256 |      25.5 | 786,432
+         9 |   512 |      12.7 | 3,145,728
+        10 |  1024 |       6.4 | 12,582,912
+        11 |  2048 |       3.2 | 50,331,648
+        12 |  4096 |       1.6 | 201,326,592
+    """
+    npix_in = arr.size
+    npix_out = hp.nside2npix(2**z_out)
+    
+    if npix_out >= npix_in:
+        error = 'Output zoom level needs to be smaller than input zoom level'
+        raise ValueError(error)
+
+    ratio = npix_in / npix_out
+    if not ratio.is_integer():  # this should never happen
+        raise ValueError(f'{ratio=}')
+    else:
+        ratio = int(ratio)
+    
+    if method == 'mean':
+        return arr.reshape(npix_out, ratio).mean(axis=-1)
+    if method == 'std':
+        return arr.reshape(npix_out, ratio).std(axis=-1)
+    if method == 'min':
+        return arr.reshape(npix_out, ratio).min(axis=-1)
+    if method == 'max':
+        return arr.reshape(npix_out, ratio).max(axis=-1)
+        
+    raise ValueError(f'{method=}')
+
+
+def _guess_gridn(da: xr.DataArray) -> str:
+    """
+    Try to guess the name of the spatial coordinate name from a list of frequent
+    options. Developed by Lukas Brunner, UHH."""
+    dims = list(da.dims)
+    gridn = []
+    if 'values' in dims:
+        gridn.append('values')
+    if 'value' in dims:
+        gridn.append('value')
+    if 'cell' in dims:
+        gridn.append('cell')
+    if 'x' in dims:
+        gridn.append('x')
+    if len(gridn) == 1:
+        return gridn[0]
+
+    raise ValueError(
+        'gridn needs to be set manually to one of: {}'.format(', '.join(dims))
+        )
+
+
 # ------------------------------------------------------------------------------
 # Remapping from the healpix grid to regular or rectilinear lat-lon grids
 # -----------------------------------------------------------------------
